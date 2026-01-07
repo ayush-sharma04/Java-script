8f32d6d544dcd864ecbb912cfc9ebabd9b6c733c
Ayush Sharma <ayush004sharma@gmail.com>
2026-01-07 16:06:07 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accoundid = 12345
+let accountemail = "ayush@gmail.com"
+var accountpassword = "12453"
+accountcity = "delhi"
+
+accoundid = 12345
+accountemail =  "karan@gmail.com"
+accountpassword = "32124"
+accountcity = "mumbai"
+
+console.table([accountid,accountemail,accountpassword,accountcity])
