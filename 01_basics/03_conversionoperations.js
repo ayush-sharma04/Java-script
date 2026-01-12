4c2c430caa5f1f666a356314e974b901e0d335e1
Ayush Sharma <ayush004sharma@gmail.com>
2026-01-12 15:45:53 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,20 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// let score = "33" => 33
+// let score = "33abc" => NaN
+// let score = "null" => 0(zero)
+// let score = "undefined" => NaN
+// let score = "true" => 1
+
+// 1 => true; 0 => false
+// "" => false
+// "ayush" => true
+
