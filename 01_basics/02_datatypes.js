0aa8a7d52ccdd5adec218e73a3e696b25615c0a2
Ayush Sharma <ayush004sharma@gmail.com>
2026-01-12 15:22:20 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+let name = "Ayush"
+let age = 21
+let isLoggedIn = false
+
+console.log(name)
+
+console.table([name,age,isLoggedIn])
+
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
