32ac2b2d0e134290045a9e36e02be00e33ea4da4
Ayush Sharma <ayush004sharma@gmail.com>
2026-01-31 14:41:09 +0000

    conversion of datatypes

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -1,11 +1,11 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // let score = "33" => 33
@@ -18,3 +18,20 @@ console.log(valueInNumber);
 // "" => false
 // "ayush" => true
 
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Ayush"
+
+let str3 = str1 + str2
+console.log(str3);
